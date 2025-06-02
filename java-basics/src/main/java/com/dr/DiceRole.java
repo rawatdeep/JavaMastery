faa7bd72305c4d0f0b491b1b4c1c884b99765ce7
Deepak Rawat <deepakrawat1907@gmail.com>
2025-06-02 16:36:49 -0400

    DiceRole

---
+++ java-basics/src/main/java/com/dr/DiceRole.java
@@ -0,0 +1,39 @@
+package com.dr;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceRole {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("How many dice would you like to roll?");
+            int numberOfDice = scanner.nextInt();
+
+            System.out.println("About to roll " + numberOfDice + " dice");
+            scanner.close();
+
+            Random rand = new Random();
+            for (int i = 0; i < numberOfDice; i++) {
+                int rolledNumber = rand.nextInt(6) + 1;
+                System.out.println(display(rolledNumber));
+            }
+        } catch (Exception e) {
+            System.out.println("That is not a valid number!");
+        }
+        
+    }
+
+    static String display(int value){
+        switch (value){
+            case 1: return "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2: return "---------\n| o     |\n|       |\n|     o |\n---------";
+            case 3: return "---------\n| o     |\n|   o   |\n|     o |\n---------";
+            case 4: return "---------\n| o   o |\n|       |\n| o   o |\n---------";
+            case 5: return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
+            case 6: return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+            default: return "Not a valid dice value";
+        }
+    }
+}
