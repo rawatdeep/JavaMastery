3258ae2f17de9e2cc7398d4e058fc90e85010ebd
AarnavR <Aarnav.Rawat@gmail.com>
2025-06-02 16:58:07 -0400

    add sortable back

--- java-basics/src/main/java/com/dr/Sortable.java
+++ java-basics/src/main/java/com/dr/Sortable.java
@@ -2,6 +2,6 @@ package com.dr;
 
 public class Sortable {
     public static void main(String[] args) {
-        System.out.println("Hello Sortable!");
+        System.out.println("Hello Sortable Again!");
     }
 }
